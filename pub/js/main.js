bc404bcebf205d63f7069727e483ac015a96022e
Slim Bouafif <slim.bouafif@gmail.com>
2013-01-18 22:03:37 +0100

    Styling it a bit. Still need to do some work on css. TODO finish writing the article

---
+++ pub/js/main.js
@@ -0,0 +1,10 @@
+var typekitBadge = 0;
+
+var removeTypekitBadge = setInterval(function() {
+  if($('img.typekit-badge').length) {
+    $('img.typekit-badge').css({display: 'none'});
+    $('img.typekit-badge').remove();
+    typekitBadge = 1;
+    clearInterval(removeTypekitBadge);
+  }
+}, 100);
