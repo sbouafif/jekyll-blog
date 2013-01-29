ca1c02c85f8171222fd59d150fcc1ef282337034
Slim Bouafif <slim.bouafif@gmail.com>
2013-01-29 04:31:11 +0100

    added article load animation. TODO improve load, add color, add blog post

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,4 +1,4 @@
-var typekitBadge = 0;
+/*var typekitBadge = 0;
 
 var removeTypekitBadge = setInterval(function() {
   if($('img.typekit-badge').length) {
@@ -11,3 +11,23 @@ var removeTypekitBadge = setInterval(function() {
 }, 100);
 
 try{Typekit.load();}catch(e){}
+*/
+
+
+$(function() {
+  $('.posts .main-picture').click(function(e) {
+    e.preventDefault();
+    var cover = $(this);
+    var article = $('.article');
+    cover.addClass('loading');
+    article.load($(this).children('h2').children('a').attr('href')+' article', function() {
+      $('.article').animate({
+        height: 'toggle'
+      }, 800, function() {
+        cover.removeClass('loading').addClass('loaded');
+        // TESTING
+        $('body').css({ 'background-color': 'red' });
+      });
+    });
+  });
+});
